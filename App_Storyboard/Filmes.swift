54942099ec4f1d40f0ba4fd233709e88f0e9c067
Aysllan00 <ayslan.conti@gec.inatel.br>
2024-10-26 18:43:34 -0400

    Projeto Quizz filmes

---
+++ App_Storyboard/Filmes.swift
@@ -0,0 +1,8 @@
+//
+//  Filmes.swift
+//  App_Storyboard
+//
+//  Created by user270446 on 10/26/24.
+//
+
+import Foundation
---
+++ PerguntasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PerguntasViewController.swift
+//  App_Storyboard
+//
+//  Created by user270446 on 10/26/24.
+//
+
+import UIKit
+
+class PerguntasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
